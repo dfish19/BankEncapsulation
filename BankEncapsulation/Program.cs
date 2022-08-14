d40117b6693cb4917d1e84fb03a1194d9adbb920
dfish19 <dfish19@yahoo.com>
2022-08-14 17:58:51 -0500

    initial

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using BankEncapsulation;
+
+var account = new BankAccount();
+
+Console.WriteLine("Deposit Amount?");
+var amountToDeposit = double.Parse(Console.ReadLine());
+account.Deposit(amountToDeposit);
+Console.WriteLine($"Thanks. Balance is now ${amountToDeposit}");
